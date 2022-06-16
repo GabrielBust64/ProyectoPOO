8e281c169d2b9b06447ded2326da71920c9bd323
Manolo <m.espineira01@ufromail.cl>
2022-06-16 17:58:18 -0400

    update

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Gei");
+        String respuesta;
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("eres gei");
+        respuesta=teclado.next();
+        System.out.println(" segun tu respuesta "+respuesta+" eres gay");
+
     }
 }
