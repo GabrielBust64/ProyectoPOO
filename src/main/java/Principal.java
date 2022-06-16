b488344874c56e1f77b90ca40ced5d6c3f7c3530
gabriel <g.bustamante01@ufromail.cl>
2022-06-16 18:46:59 -0400

    Commit de Prueba

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -2,13 +2,7 @@ import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("Gei");
-        String respuesta;
-
-        Scanner teclado = new Scanner(System.in);
-        System.out.println("eres gei");
-        respuesta=teclado.next();
-        System.out.println(" segun tu respuesta "+respuesta+" eres gay");
+        System.out.println("Hola");
 
     }
 }
